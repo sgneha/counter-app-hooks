655d9aec0dad9bf025bfa85d2a65c7397abeb4c9
sgneha <sgneha05@gmail.com>
2020-06-29 00:05:49 +0100

    first commit setup and deletion of unwanted code

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { render } from "@testing-library/react";
+
+class App extends Component {
+  render() {
+    return <h2>counter app</h2>;
+  }
+}
+
+export default App;
