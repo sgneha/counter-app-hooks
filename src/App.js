7f7d91926a28d3ae980e25c0ec778036f6bf174c
sgneha <sgneha05@gmail.com>
2020-06-29 18:58:31 +0100

    function component for counter with only increment function now

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-class App extends Component {
-  state = {
-    count: 0,
-  };
-  increment = () => {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  };
-  render() {
-    return (
-      <div>
-        <h2>counter app</h2>
-        <button onClick={this.increment}>
-          Clicked {this.state.count} times
-        </button>
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   state = {
+//     count: 0,
+//   };
+//   increment = () => {
+//     this.setState({
+//       count: this.state.count + 1,
+//     });
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <h2>counter app</h2>
+//         <button onClick={this.increment}>
+//           Clicked {this.state.count} times
+//         </button>
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+// export default App;
+
+import React, { Component, useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+};
